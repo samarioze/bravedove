dfe4ec5385b489c99e27fbfd9a45c539194cc844
Aleksey Samarin <samariaa@ya.ru>
2024-11-04 17:44:29 +0400

    Released core functions: game logic + UI: background, bird, pipes.

---
+++ src/BraveDove.java
@@ -0,0 +1,209 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
+import java.util.Objects;
+import javax.swing.*;
+import java.util.Random;
+
+public class BraveDove extends JPanel implements ActionListener, KeyListener {
+    int boardWidth = 1000;
+    int boardHeight = 640;
+
+    //images
+    Image backgroundImg;
+    Image birdImg;
+    Image topPipeImg;
+    Image bottomPipeImg;
+
+    //bird
+    int birdX = boardWidth/8;
+    int birdY = boardHeight/2;
+    int birdWidth = 68;
+    int birdHeight = 42;
+
+    class Bird {
+        int x = birdX;
+        int y = birdY;
+        int width = birdWidth;
+        int height = birdHeight;
+        Image img;
+
+        Bird(Image img) {
+            this.img = img;
+        }
+    }
+
+    int pipeX = boardWidth;
+    int pipeY = 0;
+    int pipeWidth = 64;
+    int pipeHeight = 512;
+
+    class Pipe {
+        int x = pipeX;
+        int y = pipeY;
+        int width = pipeWidth;
+        int height = pipeHeight;
+        Image img;
+        boolean passed = false;
+
+        Pipe(Image img) {
+            this.img = img;
+        }
+    }
+
+    //game logic
+    Bird bird;
+    int velocityX = -4;
+    int velocityY = 0;
+    int gravity = 1;
+
+    ArrayList<Pipe> pipes;
+    Random random = new Random();
+
+    Timer gameLoop;
+    Timer placePipesTimer;
+    boolean gameOver = false;
+    double score = 0;
+
+    BraveDove() {
+        setPreferredSize(new Dimension(boardWidth, boardHeight));
+        //setBackground(Color.blue);
+        setFocusable(true);
+        addKeyListener(this);
+
+        //load images
+        backgroundImg = new ImageIcon(Objects.requireNonNull(getClass().getResource("./bg.png"))).getImage();
+        birdImg = new ImageIcon(Objects.requireNonNull(getClass().getResource("./bird.png"))).getImage();
+        topPipeImg = new ImageIcon(Objects.requireNonNull(getClass().getResource("./toppipe.png"))).getImage();
+        bottomPipeImg = new ImageIcon(Objects.requireNonNull(getClass().getResource("./bottompipe.png"))).getImage();
+
+        //bird
+        bird = new Bird(birdImg);
+        pipes = new ArrayList<Pipe>();
+
+        //place pipes timer
+        placePipesTimer = new Timer(1500, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                placePipes();
+            }
+        });
+
+        placePipesTimer.start();
+
+        //game timer
+        gameLoop = new Timer(1000/60, this);
+        gameLoop.start();
+    }
+
+    public void placePipes() {
+        int randomPipeY = (int) (pipeY - pipeHeight/4 - Math.random()*(pipeHeight/2));
+        int openingSpase = boardHeight/4;
+
+        Pipe topPipe = new Pipe(topPipeImg);
+        topPipe.y = randomPipeY;
+        pipes.add(topPipe);
+
+        Pipe bottomPipe = new Pipe(bottomPipeImg);
+        bottomPipe.y = topPipe.y + pipeHeight + openingSpase;
+        pipes.add(bottomPipe);
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g) {
+        g.drawImage(backgroundImg, 0,0,boardWidth, boardHeight, null); //bg
+        g.drawImage(birdImg, bird.x, bird.y, birdWidth, birdHeight, null); //bird
+        //pipes
+        for (int i=0; i < pipes.size(); i++) {
+            Pipe pipe = pipes.get(i);
+            g.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height, null);
+        }
+
+        //score
+        g.setColor(Color.white);
+        g.setFont(new Font("Arial", Font.PLAIN, 32));
+        if (gameOver) {
+            g.drawString("Game Over:" + String.valueOf((int) score), 10, 35);
+        }
+        else {
+            g.drawString(String.valueOf((int) score), 10,35);
+        }
+    }
+
+    public void move () {
+        //bird
+        velocityY += gravity;
+        bird.y +=velocityY;
+        bird.y = Math.max(bird.y, 0);
+
+        //pipes
+        for (int i =0; i < pipes.size(); i++) {
+            Pipe pipe = pipes.get(i);
+            pipe.x += velocityX;
+
+            if(!pipe.passed && bird.x > pipe.x + pipe.width) {
+                pipe.passed = true;
+                score += 0.5;
+            }
+
+            if (collision(bird, pipe)) {
+                gameOver = true;
+            }
+        }
+        if (bird.y > boardHeight) {
+            gameOver = true;
+        }
+    }
+
+    public boolean collision(Bird a, Pipe b) {
+        return a.x < b.x + b.width &&
+                a.x + a.width > b.x &&
+                a.y < b.y + b.height &&
+                a.y + a.height > b.y;
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        move();
+        repaint();
+        if (gameOver) {
+            placePipesTimer.stop();
+            gameLoop.stop();
+        }
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+            velocityY = -9;
+            if (gameOver) {
+                //restart
+                bird.y = birdY;
+                velocityY = 0;
+                pipes.clear();
+                score = 0;
+                gameOver = false;
+                gameLoop.start();
+                placePipesTimer.start();
+            }
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import javax.swing.*;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello world");
+        int boardWidth = 1000;
+        int boardHeight = 562;
+
+        JFrame frame = new JFrame("Brave Dove");
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        BraveDove braveDove = new BraveDove();
+        frame.add(braveDove);
+        frame.pack();
+        braveDove.requestFocus();
+        frame.setVisible(true);
         }
     }
