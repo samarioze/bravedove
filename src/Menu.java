d8c42c699ed4439c3f8d04f6dc61a6eeb62ade23
Aleksey Samarin <samariaa@ya.ru>
2024-11-12 23:00:23 +0400

    Fixed a bug that caused the song to play in the second stream when the music button was pressed. Fixed a bug that made it impossible to turn on the music again after pressing the music off button by pressing the music on button.

--- src/Menu.java
+++ src/Menu.java
@@ -1,8 +1,10 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Objects;
 import javax.swing.*;
 
 public class Menu extends JFrame {
+    boolean musicOn = true;
     JTextField input = new JTextField("", 5);
     JLabel label = new JLabel("Your Name:");
     public Menu(){
@@ -11,7 +13,7 @@ public class Menu extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         Container container = this.getContentPane();
-        container.setLayout(new GridLayout(5,1,1,1));
+        container.setLayout(new GridLayout(8,1,1,1));
 
         container.add(label);
         container.add(input);
@@ -21,10 +23,28 @@ public class Menu extends JFrame {
         JButton buttonScore = new JButton("Scoreboard");
         container.add(buttonScore);
         JButton buttonAbout = new JButton("About");
-        container.add(buttonScore);
+        buttonAbout.addActionListener(new About());
+        container.add(buttonAbout);
+        JButton buttonMusicOff = new JButton("Music Off");
+        buttonMusicOff.addActionListener(new MusicOff());
+        container.add(buttonMusicOff);
+        JButton buttonMusicOn = new JButton("Music On");
+        buttonMusicOn.addActionListener(new MusicOn());
+        container.add(buttonMusicOn);
         JButton buttonExit = new JButton("Exit");
         buttonExit.addActionListener(new ExitGame());
         container.add(buttonExit);
+    }
+    class About implements ActionListener {
+        public void actionPerformed (ActionEvent e) {
+            String message = "Author: Aleksey Samarin\n" +
+                    "Group: Б1-ИВЧТз-32\n" +
+                    "\n" +
+                    "The game Brave Dove was created for SSTU\n" +
+                    "Discipline: Educational technological (design and technological) practice\n" +
+                    "Teacher: Lieberman Elena Ivanovna";
+            JOptionPane.showMessageDialog(null, message, "About", JOptionPane.PLAIN_MESSAGE);
+        }
     }
 
     class ExitGame implements ActionListener {
@@ -33,23 +53,53 @@ public class Menu extends JFrame {
         }
     }
 
+    class MusicOff implements ActionListener {
+        public void actionPerformed (ActionEvent e) {
+            musicOn = false;
+            MusicPlayer.stopMusic();
+            String messageMusicOff = "Music is OFF";
+            JOptionPane.showMessageDialog(null, messageMusicOff, "Music Alert", JOptionPane.PLAIN_MESSAGE);
+        }
+    }
+
+    class MusicOn implements ActionListener {
+        public void actionPerformed (ActionEvent e) {
+            if (musicOn) {
+                String messagePlaying = "Music is playing";
+                JOptionPane.showMessageDialog(null, messagePlaying, "Error turn on", JOptionPane.PLAIN_MESSAGE);
+            }
+            else {
+                musicOn = true;
+                MusicPlayer.playMusic();
+                String messageMusicOn = "Music is ON";
+                JOptionPane.showMessageDialog(null, messageMusicOn, "Music Alert", JOptionPane.PLAIN_MESSAGE);
+            }
+        }
+    }
+
     class StartGame implements ActionListener {
         public void actionPerformed (ActionEvent e) {
+            String name = input.getText();
+            if(name.trim().isEmpty()) {
+                String messageError = "Your name is not provided. Please enter your name and then try again!";
+                JOptionPane.showMessageDialog(null, messageError, "Error Name", JOptionPane.PLAIN_MESSAGE);
+            }
+            else {
+                int boardWidth = 1000;
+                int boardHeight = 562;
 
-            int boardWidth = 1000;
-            int boardHeight = 562;
+                JFrame frame = new JFrame("Brave Dove");
 
-            JFrame frame = new JFrame("Brave Dove");
+                frame.setSize(boardWidth, boardHeight);
+                frame.setLocationRelativeTo(null);
+                frame.setResizable(false);
 
-            frame.setSize(boardWidth, boardHeight);
-            frame.setLocationRelativeTo(null);
-            frame.setResizable(false);
-
-            BraveDove braveDove = new BraveDove();
-            frame.add(braveDove);
-            frame.pack();
-            braveDove.requestFocus();
-            frame.setVisible(true);
+                BraveDove braveDove = new BraveDove();
+                frame.add(braveDove);
+                frame.pack();
+                braveDove.requestFocus();
+                frame.setVisible(true);
+            }
         }
     }
 }
